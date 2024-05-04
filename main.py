50f9665388ccb9aa5cbf8df3746b9b49faaeab04
Gabriel-2511 <165502792+Gabriel-2511@users.noreply.github.com>
2024-05-04 10:56:10 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def botP(ctx):
+    await ctx.send('The bot is cool.')
+
+bot.run("Token")
